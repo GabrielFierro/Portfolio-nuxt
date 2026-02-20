9338410dc73dc490325e7c13ea2a566d597608d3
GabrielFierro <fierrogabriel13@gmail.com>
2026-02-20 17:47:54 -0300

    add Navbar component

---
+++ app/components/Navbar.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="navbar bg-base-100 shadow-sm border-b border-base-300">
+    <div class="flex-1">
+      <a class="btn btn-ghost text-xl text-primary font-bold"> GabrielFierro.dev </a>
+    </div>
+
+    <div class="flex-none gap-2">
+      <ButtonSwitcher />
+    </div>
+  </div>
+</template>
