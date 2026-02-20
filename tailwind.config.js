9fca8b9e3ef90c9d3fbce1abb089fbdd03eb68f4
GabrielFierro <fierrogabriel13@gmail.com>
2026-02-20 17:46:06 -0300

    add config for content, plugins, and daisyui theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+  content: [
+    './app.vue',
+    './components/**/*.{vue,js,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './composables/**/*.{js,ts}',
+    './plugins/**/*.{js,ts}'
+  ],
+  theme: {},
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          primary: '#794DFF',
+          secondary: '#4F8CFF',
+          accent: '#FF4FD8',
+          neutral: '#1A1A1A',
+          'base-100': '#F8F9FC',
+          'base-200': '#F1F3F9',
+          'base-300': '#E5E7F0',
+          'base-content': '#1A1A1A',
+          info: '#3ABFF8',
+          success: '#22C55E',
+          warning: '#F59E0B',
+          error: '#EF4444'
+        }
+      }
+    ]
+  }
 }
-
