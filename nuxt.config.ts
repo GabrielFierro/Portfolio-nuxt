3f0d72d13a085dc3bf47ac4c7b9f843d1a2b9bee
GabrielFierro <fierrogabriel13@gmail.com>
2026-02-20 14:58:50 -0300

    add nuxt configuration for i18n, tailwind, and SEO

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+	compatibilityDate: '2025-07-15',
+	devtools: { enabled: true },
+	modules: ['@nuxtjs/i18n', '@nuxtjs/tailwindcss', '@nuxtjs/seo', '@nuxtjs/content'],
+	i18n: {
+		locales: [
+			{ code: 'en', language: 'en-US' },
+			{ code: 'es', language: 'es-ES' },
+		],
+		defaultLocale: 'en',
+	},
+});
