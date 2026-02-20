1e6931ec9fbb46258c35a2ec590314e20e996ff5
GabrielFierro <fierrogabriel13@gmail.com>
2026-02-20 17:45:32 -0300

    add configuration for modules, i18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	compatibilityDate: '2025-07-15',
-	devtools: { enabled: true },
-	modules: ['@nuxtjs/i18n', '@nuxtjs/tailwindcss', '@nuxtjs/seo', '@nuxtjs/content'],
-	i18n: {
-		locales: [
-			{ code: 'en', language: 'en-US' },
-			{ code: 'es', language: 'es-ES' },
-		],
-		defaultLocale: 'en',
-	},
-});
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+
+  modules: ['@nuxtjs/i18n', '@nuxtjs/tailwindcss', '@nuxtjs/seo'],
+
+  i18n: {
+    strategy: 'prefix_except_default',
+    defaultLocale: 'en',
+    langDir: 'locales/',
+    lazy: true,
+    detectBrowserLanguage: false,
+    locales: [
+      { code: 'en', iso: 'en-US', file: 'en.json' },
+      { code: 'es', iso: 'es-ES', file: 'es.json' }
+    ]
+  }
+})
