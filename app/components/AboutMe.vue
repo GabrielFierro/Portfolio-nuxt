c7329e7751194f81d2f04b1df0c5c354a4f63727
GabrielFierro <fierrogabriel13@gmail.com>
2026-02-23 12:11:27 -0300

    add AboutMe section to index

---
+++ app/components/AboutMe.vue
@@ -0,0 +1,9 @@
+<script lang="ts"></script>
+
+<template>
+  <section id="about" className="py-24">
+    <div className="text-center">
+      <span className="text-5xl font-bold border-b-2 border-primary">About Me</span>
+    </div>
+  </section>
+</template>
