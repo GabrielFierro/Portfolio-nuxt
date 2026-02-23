260e8ed867df22c84109e828291cfbe48e35fc72
GabrielFierro <fierrogabriel13@gmail.com>
2026-02-23 13:53:14 -0300

    add a component to display my technologies

---
+++ app/components/TechnologiesBadge.vue
@@ -0,0 +1,64 @@
+<template>
+  <div className="flex flex-wrap items-center justify-center gap-4 mx-16">
+    <span
+      class="inline-flex items-center rounded-md bg-gray-400/15 px-2 py-1 text-md font-medium text-slate-800"
+      >JavaScript</span
+    >
+    <span
+      class="inline-flex items-center rounded-md bg-gray-400/15 px-2 py-1 text-md font-medium text-slate-800"
+      >Vue</span
+    >
+    <span
+      class="inline-flex items-center rounded-md bg-gray-400/15 px-2 py-1 text-md font-medium text-slate-800"
+      >React</span
+    >
+    <span
+      class="inline-flex items-center rounded-md bg-gray-400/15 px-2 py-1 text-md font-medium text-slate-800"
+      >TypeScript</span
+    >
+    <span
+      class="inline-flex items-center rounded-md bg-gray-400/15 px-2 py-1 text-md font-medium text-slate-800"
+      >Node.js</span
+    >
+    <span
+      class="inline-flex items-center rounded-md bg-gray-400/15 px-2 py-1 text-md font-medium text-slate-800"
+      >Express</span
+    >
+    <span
+      class="inline-flex items-center rounded-md bg-gray-400/15 px-2 py-1 text-md font-medium text-slate-800"
+      >NestJs</span
+    >
+    <span
+      class="inline-flex items-center rounded-md bg-gray-400/15 px-2 py-1 text-md font-medium text-slate-800"
+      >PostgreSQL</span
+    >
+    <span
+      class="inline-flex items-center rounded-md bg-gray-400/15 px-2 py-1 text-md font-medium text-slate-800"
+      >MySQL</span
+    >
+    <span
+      class="inline-flex items-center rounded-md bg-gray-400/15 px-2 py-1 text-md font-medium text-slate-800"
+      >Cypress</span
+    >
+    <span
+      class="inline-flex items-center rounded-md bg-gray-400/15 px-2 py-1 text-md font-medium text-slate-800"
+      >Tailwind</span
+    >
+    <span
+      class="inline-flex items-center rounded-md bg-gray-400/15 px-2 py-1 text-md font-medium text-slate-800"
+      >Material UI</span
+    >
+    <span
+      class="inline-flex items-center rounded-md bg-gray-400/15 px-2 py-1 text-md font-medium text-slate-800"
+      >Git</span
+    >
+    <span
+      class="inline-flex items-center rounded-md bg-gray-400/15 px-2 py-1 text-md font-medium text-slate-800"
+      >GitHub</span
+    >
+    <span
+      class="inline-flex items-center rounded-md bg-gray-400/15 px-2 py-1 text-md font-medium text-slate-800"
+      >Figma</span
+    >
+  </div>
+</template>
