bb24b9c86684f908720767f17791b414302f9d86
GabrielFierro <fierrogabriel13@gmail.com>
2026-03-16 20:07:20 -0300

    fix ring for each circle, fix compan, and period

--- app/components/sections/ExperienceTimeline.vue
+++ app/components/sections/ExperienceTimeline.vue
@@ -21,7 +21,7 @@
       <div class="relative border-l border-gray-300 dark:border-gray-700">
         <div v-for="(exp, index) in experiences" :key="index" class="mb-12 ml-6">
           <span
-            class="absolute -left-3 flex items-center justify-center w-6 h-6 bg-primary rounded-full ring-8 ring-white dark:ring-gray-950"
+            class="absolute -left-3 flex items-center justify-center w-6 h-6 bg-primary rounded-full ring-8 ring-gray-100 dark:ring-gray-950"
           />
 
           <h3 class="text-xl font-semibold dark:text-white">
@@ -29,11 +29,11 @@
           </h3>
 
           <p class="text-primary font-medium">
-            {{ t(exp.company) }}
+            {{ exp.company }}
           </p>
 
           <time class="block mb-3 text-sm text-gray-500">
-            {{ t(exp.period) }}
+            {{ exp.period }}
           </time>
 
           <p class="text-gray-600 dark:text-gray-400 mb-4 whitespace-pre-line">
