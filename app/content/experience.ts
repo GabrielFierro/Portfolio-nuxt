f010e5ea594e6da278829b49579d0186f2ef06f1
GabrielFierro <fierrogabriel13@gmail.com>
2026-03-16 18:59:57 -0300

    add experience data

---
+++ app/content/experience.ts
@@ -0,0 +1,61 @@
+export const experiences = [
+  {
+    company: 'Meliorar',
+    role: 'experience.meliorarRole',
+    period: 'Aug 2025 — Present',
+    description: 'experience.meliorarDescription',
+    fullDescription: 'experience.meliorarFullDescription',
+    tech: [
+      'Product Strategy',
+      'UX/UI',
+      'Figma',
+      'React',
+      'TypeScript',
+      'Node.js',
+      'Express.js',
+      'PostgreSQL',
+      'PrismaORM',
+      'Git',
+      'GitHub',
+      'Jira'
+    ]
+  },
+  {
+    company: 'Travellyze',
+    role: 'experience.dataAnalystRole',
+    period: 'Dec 2024 — Oct 2025',
+    description: 'experience.dataAnalystDescription',
+    fullDescription: 'experience.dataAnalystFullDescription',
+    tech: ['Python', 'Pandas', 'NumPy', 'Google Colab', 'Git', 'GitHub', 'Jira', 'Slack']
+  },
+  {
+    company: 'Travellyze',
+    role: 'experience.fullstackRole',
+    period: 'Dec 2023 — Oct 2025',
+    description: 'experience.fullstackDescription',
+    fullDescription: 'experience.fullstackFullDescription',
+    tech: [
+      'Vue 2/3',
+      'Quasar',
+      'Pinia',
+      'XState',
+      'Vitest',
+      'NestJS',
+      'MySQL',
+      'Cypress',
+      'Firebase',
+      'Git',
+      'GitHub',
+      'Jira',
+      'Slack'
+    ]
+  },
+  {
+    company: 'Code In Magenta',
+    role: 'experience.frontendRole',
+    period: 'Aug 2020 — Oct 2020',
+    description: 'experience.frontendDescription',
+    fullDescription: 'experience.frontendFullDescription',
+    tech: ['HTML', 'CSS', 'JavaScript', 'Tailwind']
+  }
+]
