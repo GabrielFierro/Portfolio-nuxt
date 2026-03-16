a4237dea6f3150071dc394c0b385411b313c1a29
GabrielFierro <fierrogabriel13@gmail.com>
2026-03-16 15:59:08 -0300

    add DarkModeToggle component and improve classes

--- app/components/Navbar.vue
+++ app/components/Navbar.vue
@@ -1,11 +1,14 @@
 <template>
-  <div class="navbar bg-base-100 fixed top-0 z-50 px-6">
+  <div
+    class="w-full flex items-center justify-between bg-white/80 dark:bg-black/60 backdrop-blur-md border-b border-gray-200 dark:border-white/10 fixed top-0 z-50 px-6 py-3"
+  >
     <div class="flex-1">
-      <a class="btn btn-ghost text-xl text-black font-bold"> GabrielFierro.dev </a>
+      <a class="text-xl font-bold text-black dark:text-gray-200"> GabrielFierro.dev </a>
     </div>
 
-    <div class="flex-none gap-2 mr-16">
+    <div class="flex items-center gap-4">
       <ButtonSwitcher />
+      <DarkModeToggle />
     </div>
   </div>
 </template>
