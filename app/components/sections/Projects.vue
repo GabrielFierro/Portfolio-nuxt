4d8190eb95f4f80cdc767230e281b1fd5def2c3e
GabrielFierro <fierrogabriel13@gmail.com>
2026-03-16 19:01:57 -0300

    add Projects section

---
+++ app/components/sections/Projects.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+  import { projects } from '@/content/projects'
+</script>
+
+<template>
+  <section id="projects" class="py-24 px-6 bg-gray-50 dark:bg-backgroundDark">
+    <div class="max-w-6xl mx-auto">
+      <h2 class="text-5xl font-bold text-center mb-16 dark:text-white">Projects</h2>
+
+      <div class="grid md:grid-cols-2 lg:grid-cols-2 gap-8">
+        <UiProjectCard v-for="project in projects" :key="project.title" v-bind="project" />
+      </div>
+    </div>
+  </section>
+</template>
