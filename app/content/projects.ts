687b0caa0d466c31f3224e37fefd23761b948e09
GabrielFierro <fierrogabriel13@gmail.com>
2026-03-16 19:00:06 -0300

    add projects data

---
+++ app/content/projects.ts
@@ -0,0 +1,57 @@
+export const projects = [
+  {
+    title: 'Code In Magenta Landing Page',
+    description:
+      'Responsive landing page developed for Code In Magenta using modern frontend practices and a mobile-first approach.',
+    tech: ['JavaScript', 'Tailwind CSS', 'HTML'],
+    type: 'Client Project',
+    github: 'https://github.com/magentateam/landingOng',
+    demo: 'https://magentateam.github.io/landingOng/public/',
+    featured: true
+  },
+  {
+    title: 'Weather App',
+    description:
+      'Weather application that allows users to search for cities and view real-time weather data through an external API.',
+    tech: ['JavaScript', 'API Integration', 'CSS'],
+    type: 'Frontend Project',
+    github: 'https://github.com/GabrielFierro/Weather-app',
+    demo: 'https://weather-app-taupe-rho.vercel.app/'
+  },
+  {
+    title: 'Travelling Landing Page',
+    description:
+      'Travel-themed landing page focused on layout design, responsive UI, and modern web styling techniques.',
+    tech: ['HTML', 'CSS', 'JavaScript'],
+    type: 'Frontend Project',
+    github: 'https://github.com/GabrielFierro/Travelling-landing-page',
+    demo: 'https://travelling-landing-page.vercel.app/'
+  },
+  {
+    title: 'Advice Generator',
+    description:
+      'Interactive application that fetches random advice from a public API and displays it dynamically.',
+    tech: ['JavaScript', 'API', 'Responsive UI'],
+    type: 'Frontend Project',
+    github: 'https://github.com/GabrielFierro/advice-generator',
+    demo: 'https://advice-generator-two-gold.vercel.app/'
+  },
+  {
+    title: 'Sunnyside Landing Page',
+    description:
+      'I built this Landing Page base on a challenge from Frontend Mentor using React. I use it to make reusables components to avoid repeat code.',
+    tech: ['JavaScript', 'API', 'Responsive UI'],
+    type: 'Frontend Project',
+    github: 'https://github.com/GabrielFierro/Sunnyside-agency-landing-page',
+    demo: 'https://resonant-kringle-6016a6.netlify.app/'
+  },
+  {
+    title: 'Loopstudios Landing Page',
+    description:
+      'I built this Landing Page base on a challenge from Frontend Mentor using HTML, CSS, and JavaScript making focus on the images section made with grid.',
+    tech: ['JavaScript', 'API', 'Responsive UI'],
+    type: 'Frontend Project',
+    github: 'https://github.com/GabrielFierro/loopstudios-landing-page',
+    demo: 'https://loopstudios-landing-page-jade.vercel.app/'
+  }
+]
